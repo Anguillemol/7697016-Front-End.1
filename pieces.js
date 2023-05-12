a66f95abc342104862e33c54fbf34fe49107d517
Anguillemol <120462310+Anguillemol@users.noreply.github.com>
2023-05-12 16:29:05 +0200

    z

---
+++ pieces.js
@@ -0,0 +1,41 @@
+//Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
+for (let i = 0; i < pieces.length; i++){
+
+
+    const article = pieces[i]
+
+    const sectionFiches = document.querySelector(".fiches");
+
+    const pieceElement = document.createElement("article")
+
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "Aucune catégorie";
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment"
+
+    const disponibiliteElement = document.createElement("p");
+    disponibiliteElement.innerText = article.disponibilite = true ? "En stock" : "Rupture de stock"
+
+    sectionFiches.appendChild(pieceElement)
+
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(disponibiliteElement);
+
+
+}
